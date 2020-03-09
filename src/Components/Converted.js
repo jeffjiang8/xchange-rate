0665a711ea3a2718e52505768e4ca0f00d2dcd0b
Jeff Jiang <jeffjiang8@gmail.com>
2020-03-09 17:25:53 -0400

    Got the image

---
+++ src/Components/Converted.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Converted extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Converted;
---
+++ src/Containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Currency from '../Components/Currency'
+import Converted from '../Components/Converted'
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home">
+                <img src='../images/mula.gif' className="app-logo" />
+            </div>
+        );
+    }
+}
+
+export default Home;
