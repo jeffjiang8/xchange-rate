bd48b1a33c0674fce7960e3177bbd953575405c7
Jeff Jiang <jeffjiang8@gmail.com>
2020-03-11 18:33:11 -0400

    progress

--- src/Components/Currency.js
+++ src/Components/Currency.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class Currency extends Component {
     render() {
+        console.log(this.props.allOptions)
         return (
             <div className="currency">
                 <form>
                     <select>
-                        <option>1</option>
+                        <option>Select Your Currency</option>
                         <option>2</option>
                         <option>3</option>
                         <option>4</option>
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -4,22 +4,32 @@ import Converted from '../Components/Converted'
 
 class Home extends Component {
 
-    
+    state = {
+        allCurrency: '',
+        selectedCurrency: ''
+    }
 
     componentDidMount() {
-        const key = process.env.REACT_APP_XCHANGE_API_KEY
-        fetch(`https://prime.exchangerate-api.com/v5/${key}/latest/CNY`)
+        // const key = process.env.REACT_APP_XCHANGE_API_KEY
+        // fetch(`https://prime.exchangerate-api.com/v5/${key}/latest/CNY`)
+        // .then(resp => resp.json())
+        // .then(console.log)
+        fetch('https://openexchangerates.org/api/currencies.json')
         .then(resp => resp.json())
-        .then(console.log)
+        .then(json => this.setState({ allCurrency: json }))
+    }
+
+    handleSelected = () => {
+        console.log("selected")
     }
 
     render() {
-        
+        console.log(this.state.allCurrency)
         return (
             <div className="home">
                 <img src='../images/mula.gif' className="app-logo" alt="meh"/>
                 <div className="actions">
-                    <Currency />
+                    <Currency allOptions={this.state.allCurrency}/>
                     <div className="btns">
                         <button><img src="../images/swap.png" alt="meh" className="swap-btn"/></button>
                     </div>
