a7c0a3e22e81fbfd0703a8f57142851aef577221
Jeff Jiang <jeffjiang8@gmail.com>
2020-03-18 15:42:58 -0400

    werd

--- src/Components/Converted.js
+++ src/Components/Converted.js
@@ -4,7 +4,15 @@ class Converted extends Component {
     render() {
         return (
             <div className="converted">
-                <p style={{"color": "white"}}>converted</p>
+                <form>
+                    <select>
+                        <option>Select Your Currency</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                </form>
             </div>
         );
     }
