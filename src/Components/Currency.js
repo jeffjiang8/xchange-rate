feef2eeb1849da81cf6c0fe65c40574219f67c36
Jeff Jiang <jeffjiang8@gmail.com>
2020-04-06 16:09:24 -0400

    werd

--- src/Components/Currency.js
+++ src/Components/Currency.js
@@ -3,27 +3,26 @@ import React, { Component } from 'react';
 class Currency extends Component {
 
     state = {
-        allCurrency: {},
+        allCurrencies: null,
         selectedCurrency: '',
         loading: true
     }
 
-    // componentDidMount() {
-    //     fetch('https://openexchangerates.org/api/currencies.json')
-    //     .then(resp => resp.json())
-    //     .then(json => this.setState({ allCurrency: json, loading: false }))
-    // }
+    componentDidMount() {
+        fetch('https://openexchangerates.org/api/currencies.json')
+        .then(resp => resp.json())
+        .then(json => this.setState({ allCurrencies: json, loading: false }))
+    }
 
     render() {
-        console.log(this.props.currencies)
+        console.log(this.state.allCurrencies)
+        if (this.state.loading) {
+            return <p>loading...</p>
+        }
         return (
             <div className="currency">
-                {   
-                    this.props.loading
-                    ?
-                    ''
-                    :
-                    this.props.currencies.map((currency, index) => {
+                { 
+                    this.state.allCurrencies.map((currency, index) => {
                         return (
                             <form>
                                 <select>
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -11,15 +11,15 @@ class Home extends Component {
         loading: true
     }
 
-    componentDidMount() {
-    //     // const key = process.env.REACT_APP_XCHANGE_API_KEY
-    //     // fetch(`https://prime.exchangerate-api.com/v5/${key}/latest/CNY`)
-    //     // .then(resp => resp.json())
-    //     // .then(console.log)
-        fetch('https://openexchangerates.org/api/currencies.json')
-        .then(resp => resp.json())
-        .then(json => this.setState({ allCurrency: json, loading: false }))
-    }
+    // componentDidMount() {
+    // //     // const key = process.env.REACT_APP_XCHANGE_API_KEY
+    // //     // fetch(`https://prime.exchangerate-api.com/v5/${key}/latest/CNY`)
+    // //     // .then(resp => resp.json())
+    // //     // .then(console.log)
+    //     fetch('https://openexchangerates.org/api/currencies.json')
+    //     .then(resp => resp.json())
+    //     .then(json => this.setState({ allCurrency: json, loading: false }))
+    // }
 
     handleSelected = () => {
         console.log("selected")
